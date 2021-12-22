c8510576ee2bb82c638dab4b6fe2b4c5e457edae
Abdulmalik Sanni <muftisanni@gmail.com>
2021-12-22 01:02:50 +0100

    Add humidty to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ callback('Unable to connect to weather service!', undefined)
 } else if (body.error) {
 callback('Unable to find location', undefined)
 } else {
-callback(undefined, body.current.weather_descriptions[0] + ". it is currently "  + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " % chance of rain.")
+
+callback(undefined, body.current.weather_descriptions[0] + ". it is currently "  + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + "The humidity is" + body.current.humidity + " %. ")
 }
 })
     
